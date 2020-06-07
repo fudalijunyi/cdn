c0e036a0a73450120dd97913559b83a2bc1981f4
lovelijunyi <lijunyi088@163.com>
2020-06-07 18:29:09 +0800

    up

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+# 拉取远程仓库到本地
+#os.system('git pull origin master')
+#os.system('git add .')
+#os.system('''git commit -m '描述内容' ''')
+# 推送到GitHub仓库
+#os.system('git push origin master')
+
+
+
+
+
+
+
+
+
+
